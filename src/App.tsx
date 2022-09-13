540d5075430b338740f5e1fbfcea1e7a6f5d1204
lofti198 <isolar2005@gmail.com>
2022-09-13 12:01:14 +0300

    fixes for template before fork

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import React, { useEffect } from "react";
 import { injectOnPageLoad } from "./libs/inject";
-
+import html2canvas from "html2canvas";
 function getLogo() {
   if (window.chrome) {
     return window.chrome.runtime.getURL(logo);
@@ -22,16 +22,17 @@ function App() {
       <header className="App-header">
         <button
           onClick={() => {
-            document.dispatchEvent(
-              new CustomEvent("seek_to_event", {
-                detail: { seconds: 15 },
-              })
-            );
-            document.dispatchEvent(new CustomEvent("pause_video"));
-            console.log("onclick");
+            // document.dispatchEvent(
+            //   new CustomEvent("seek_to_event", {
+            //     detail: { seconds: 15 },
+            //   })
+            // );
+            // document.dispatchEvent(new CustomEvent("pause_video"));
+
+            console.log("onclick1");
           }}
         >
-          Tap
+          Tap1
         </button>
       </header>
     </div>
